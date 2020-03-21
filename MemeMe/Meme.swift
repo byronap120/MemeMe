1aea3c3e102ad0827fb2084ef7afe819ede4f5b9
byronajin <byronap120@gmail.com>
2020-03-21 17:19:10 -0600

    File for Meme Struct

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Byron Ajin on 3/21/20.
+//  Copyright © 2020 Byron Ajin. All rights reserved.
+//
+
+import Foundation
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
