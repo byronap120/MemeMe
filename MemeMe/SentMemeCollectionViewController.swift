c263bc91963a6a416c7808b50effb34bad3b79fe
byronajin <byronap120@gmail.com>
2020-03-22 13:23:52 -0600

    Storyboard Tab Bar
    - View controller for table
    - View controller for collection

---
+++ MemeMe/SentMemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Byron Ajin on 3/22/20.
+//  Copyright © 2020 Byron Ajin. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewController : UIViewController {
+    
+}
---
+++ MemeMe/SentMemesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Byron Ajin on 3/22/20.
+//  Copyright © 2020 Byron Ajin. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeTableViewController : UIViewController {
+    
+}
