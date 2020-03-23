ef484836cc2520a57089530e512cfd9273952343
byronajin <byronap120@gmail.com>
2020-03-22 19:16:39 -0600

    CollectionView

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Byron Ajin on 3/22/20.
+//  Copyright © 2020 Byron Ajin. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var memeTitle: UILabel!
+    @IBOutlet weak var memeImageView: UIImageView!
+}
--- MemeMe/SentMemeCollectionViewController.swift
+++ MemeMe/SentMemeCollectionViewController.swift
@@ -8,6 +8,42 @@
 
 import UIKit
 
-class SentMemeCollectionViewController : UIViewController {
+class SentMemeCollectionViewController : UICollectionViewController {
+    
+    @IBOutlet var memeCollectionView: UICollectionView!
+    
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        memeCollectionView.reloadData()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeViewCell", for: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        cell.memeTitle.text = "\(meme.topText)...\(meme.bottomText)"
+        cell.memeImageView.image = meme.memedImage
+
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+//        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
+//        detailController.villain = self.allVillains[(indexPath as NSIndexPath).row]
+//        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }
     
 }
